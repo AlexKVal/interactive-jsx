410c214965063a8ee0839abc9b582edd4f899dc8
AlexKVal <alexkval@gmail.com>
2015-03-09 19:30:14 +0200

    working version

--- js/code.jsx
+++ js/code.jsx
@@ -1,45 +1,54 @@
 // var React = require('react');
+var initialOutputText = '...code after JSXTransform';
 
 var Convertor = React.createClass({
   getInitialState: function() {
     return {
-      input: '/* jsx code here */',
-      output: 'here we have output code',
+      input: '',
+      output: initialOutputText,
       err: ''
     };
   },
   update: function (event) {
     var jxsCode = event.target.value;
-    try {
+    if (jxsCode === '') {
       this.setState({
-        output: JSXTransformer.transform(jxsCode).code
+        output: initialOutputText
       });
-    } catch (error) {
-      this.setState({
-        err: error.message
-      });
+    } else {
+      try {
+        this.setState({
+          output: JSXTransformer.transform(jxsCode).code,
+          err: ''
+        });
+      } catch (error) {
+        this.setState({
+          err: error.message
+        });
+      }
     }
   },
   render: function() {
     return (
       <div className="convertor">
-        <div className="error bg-danger">&nbsp; text of error </div>
+        <div className="row">
+          <p className="alert alert-danger"> &nbsp; {this.state.err} </p>
+        </div>
 
         <div className="container-fluid">
-          <div className="row well">
-            <div className="jsx-code col-md-6">
-              <textarea defaultValue={this.state.input} className="form-control" name="jsx" rows="20"></textarea>
-            </div>
-            <div className="js-code col-md-6">
-              <textarea value={this.state.output} readOnly className="form-control" name="js" rows="20"></textarea>
-            </div>
+          <div className="row">
+            <textarea
+              defaultValue={this.state.input}
+              onChange={this.update}
+              placeholder="JSX code here..."
+              className="input-lg col-md-6">
+            </textarea>
+            <pre className="input-lg col-md-6">{this.state.output}</pre>
           </div>
         </div>
       </div>
     );
   }
 });
-
-
 
 React.render(<Convertor />, document.body);
